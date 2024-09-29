9114d9d06323e230c1c3165852175c3433cc871f
devtushar001 <tc341269@gmail.com>
2024-09-29 05:23:04 -0500

    getting single product from the mongodb

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,6 +1,7 @@
 // GET ALL PRODUCT CONTROLLER
 
 import productModel from "../models/productModel.js";
+import userModel from "../models/userModel.js";
 
 export const getAllProductController = async (req, res) => {
     try {
@@ -16,6 +17,37 @@ export const getAllProductController = async (req, res) => {
             success: false,
             message: "Error in get all products api",
             error
+        })
+    }
+}
+
+export const getSingleProductController = async (req, res) => {
+    try {
+        const singleProduct = await productModel.findById(req.params.id);
+        // validation 
+        if(!singleProduct) {
+            return res.status(500).send({
+                success: false,
+                message: "single product not found",
+                singleProduct
+            })
+        } 
+
+        res.status(200).send({
+            success: true,
+            message: "Single product found Successfully"
+        })
+    } catch (error) {
+        if(error.name === 'CastError') {
+            return res.status(500).send({
+                success: false,
+                message: 'Cast Error You should resolve product id in params'
+            })
+        }
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'single product api error not found'
         })
     }
 }
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getAllProductController } from '../controllers/productController.js';
+import { getAllProductController, getSingleProductController } from '../controllers/productController.js';
 const productRouter = express.Router();
 
 // routes
 productRouter.get('/get-all', getAllProductController);
+
+// get single product
+productRouter.get('/:id', getSingleProductController);
 
 export default productRouter;
